3037d8375a1ffa6649cb8b52a926a5f9210ee525
Shreyash Maskar <shreyashmaskar5359@gmail.com>
2024-08-20 17:54:15 +0530

    rotate array

---
+++ 3.Arrays/easy/5.rotate_array.py
@@ -0,0 +1,53 @@
+# def left_rotate(arr,pos):
+#     n=len(arr)
+#     pos=pos%n
+#     arr2=[]
+#     for i in range(n):
+#         arr2.append(arr[(i+pos)%n])
+#     return arr2
+
+# def right_rotate(arr,pos):
+#     n=len(arr)
+#     pos=pos%n
+#     arr2=[0]*n
+#     for i in range(n):
+#         arr2.append(arr[(i-pos)%n])
+#     return arr2
+
+# without using extra space : Leetcode 189
+def right_rotate(nums,k):
+    n = len(nums)
+    k = k % n
+    
+    def reverse_section(start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+    
+    reverse_section(0, n - 1)
+    reverse_section(0, k - 1)
+    reverse_section(k, n - 1)
+    return nums
+
+def left_rotate(nums,k):
+    n = len(nums)
+    k = k % n
+    
+    def reverse_section(start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+    reverse_section(0,n-1)
+    reverse_section(0,n-k-1)
+    reverse_section(n-k,n-1)
+    return nums
+
+
+array=[1,2,3,4,5,6,7,8,9,0]
+# array=[-1,-100,3,99]
+pos=3
+a=left_rotate(array,pos)
+# b=right_rotate(array,pos)
+print(a)
--- 3.Arrays/easy/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-nums=[0,0,1,1,1,2,2,3,3,4]
-# nums= sorted(set(nums))
-# print(nums)
