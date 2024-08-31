7c67bf33beb68eb9e1b1d9665b3d0759df610ac7
Shreyash Maskar <shreyashmaskar5359@gmail.com>
2024-08-31 23:25:21 +0530

    sort array of 0,1,2

---
+++ 3.Arrays/medium/2.sort_arrays_of_012.py
@@ -0,0 +1,22 @@
+# Leetcode 75
+def SortArry(nums):
+    n=len(nums)
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+    
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+            
+    return nums
+    
+arr=[2,0,2,1,1,0]
+print(SortArry(arr))
