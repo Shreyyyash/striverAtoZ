f3425589e85e82c0cb169725ee7cc7c92c6dc342
Shreyash Maskar <shreyashmaskar5359@gmail.com>
2024-09-04 22:44:05 +0530

    modified medium 6

--- 3.Arrays/medium/6.Rearrange Array Elements by Sign.py
+++ 3.Arrays/medium/6.Rearrange Array Elements by Sign.py
@@ -1,22 +1,34 @@
 def rearrangeArray(nums):
     n=len(nums)
-    arr1=[]
-    arr2=[]
-    arr3=n*[]
-    count=0
+    ans = [0]*n
+    positive,negative=0,1
     for i in range(n):
         if nums[i]>0:
-            arr1.append(nums[i])
+            ans[positive]=nums[i]
+            positive+=2
         else:
-            arr2.append(nums[i])
-    while count <n//2:
-        arr3.append(arr1[count])
-        arr3.append(arr2[count])
-        count+=1
-
-    return arr3
-
+            ans[negative]=nums[i]
+            negative+=2
+    
+    return ans
+    
 
 nums=[3,1,-2,-5,2,-4]
 a=rearrangeArray(nums)
 print(a)
+
+# arr1=[]
+#     arr2=[]
+#     arr3=n*[]
+#     count=0
+#     for i in range(n):
+#         if nums[i]>0:
+#             arr1.append(nums[i])
+#         else:
+#             arr2.append(nums[i])
+#     while count <n//2:
+#         arr3.append(arr1[count])
+#         arr3.append(arr2[count])
+#         count+=1
+
+#     return arr3
